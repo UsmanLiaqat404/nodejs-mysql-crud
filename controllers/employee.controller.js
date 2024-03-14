a3d2cbda13207a123f921fa5a0627175e43fcc38
Usman Liaqat <memerboyemiology444@gmail.com>
2024-03-15 02:39:22 +0500

    Add employee CRUD operations and error handling (completed 🙌)

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,14 +1,57 @@
 const express = require("express");
 const router = express.Router();
 
+const service = require("../services/employee.service");
+
 const db = require("../db");
 
+// Get all employees
 router.get("/", async (req, res) => {
-  const [rows] = await db.query("SELECT * FROM employees").catch((err) => {
-    res.json(err);
-  });
+  const employees = await service.getAllEmployees();
+  res.send(employees);
+});
 
-  res.json(rows);
+// Get employee by id
+router.get("/:id", async (req, res) => {
+  const [employee] = await service.getEmployeeById(req.params.id);
+  console.log(employee);
+  if (employee === undefined)
+    return res
+      .status(404)
+      .send("Employee not found with the given id : " + req.params.id);
+
+  res.send(employee);
+});
+
+// delete employee
+router.delete("/:id", async (req, res) => {
+  const employee = await service.getEmployeeById(req.params.id);
+  if (employee.length === 0)
+    return res
+      .status(404)
+      .send("Employee not found with the given id : " + req.params.id);
+
+  await service.deleteEmployee(req.params.id);
+  res.send("Employee deleted successfully");
 });
 
 module.exports = router;
+
+// add employee
+router.post("/", async (req, res) => {
+  await service.addOrEditEmployee(req.body);
+
+  res.status(201).send("Employee added successfully");
+});
+
+// edit employee
+router.put("/:id", async (req, res) => {
+  const employee = await service.getEmployeeById(req.params.id);
+  if (employee.length === 0)
+    return res
+      .status(404)
+      .send("Employee not found with the given id : " + req.params.id);
+
+  await service.addOrEditEmployee(req.body, req.params.id);
+  res.send("Employee updated successfully");
+});
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+require("express-async-errors");
 
 const db = require("./db");
 
 const employeeRoutes = require("./controllers/employee.controller");
 
+app.use(bodyParser.json());
 app.use("/api/employees", employeeRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Employee CRUD API made with Node.js and MySQL. ");
+});
+
+app.use((err, req, res, next) => {
+  console.error("Error: ", err);
+  res.status(err.status || 500).send("Something went wrong!");
 });
 
 db.query("SELECT 1")
